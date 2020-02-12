8ce96a71986a736d11bf03688870e07017877a5f
RomanKrajewski <s0553506@htw-berlin.de>
2020-02-12 13:23:05 +0100

    add some example questions and make the game work

--- scripts/main.js
+++ scripts/main.js
@@ -2,118 +2,110 @@ import {situations} from "./situations.js";
 
 window.onload = introduction()
 
-let situation = 0
+let currentSituation = 0
 let code = ""
 
 /* Functions for game play */
 
 function introduction() {
-  displayInstructions("Welcome to TITLE! We want to make this game a good experience for our target groups."
-  + "For that we need your help. Please play this game and fill out our questionnaire afterwards.") //Add title
-  displayText("Blabla") //TODO add intro text
-  createNextButton()
+    displayText("Welcome to TITLE! We want to make this game a good experience for our target groups."
+        + "For that we need your help. Please play this game and fill out our questionnaire afterwards.");
+    createContinueButton()
 }
 
 function nextSituation() {
-  if(situation == 0) removeTextFromElement("instructions")
-  if(!situations.hasOwnProperty(situation)) {
-    end()
-  }
-  displayText(situations[situation].text)
-  createOptionButtons(situations[situation].options)
-  situation++
+    if (currentSituation >= situations.length) {
+        end();
+        return;
+    }
+    clearText();
+    clearOptions();
+    displayText(situations[currentSituation].text)
+    displayOptions(situations[currentSituation].options)
+    currentSituation++
 }
 
 function end() {
-  removeTextFromElement("instructions")
-  displayInstructions("Thank you for playing! Please help us improve our game by answering a few questions!"
-  + "In order to do so, please copy the code displayed below,"
-  + "open the questionnaire by clicking on the button (opens in a new tab)"
-  + "and paste the code into the appropriate field on the first page of the questionnaire.")
-  displayInstructions(code, "code", True)
-  addLink("https://example.com", "Magically take me to the questionnaire") //TODO Update link
+    clearText();
+    clearOptions();
+    displayText("Thank you for playing! Please help us improve our game by answering a few questions!\n"
+        + "In order to do so, please copy the code displayed below,"
+        + "open the questionnaire by clicking on the button (opens in a new tab)"
+        + "and paste the code into the appropriate field on the first page of the questionnaire.")
+    displayText(code);
+    addLink("https://example.com", "Magically take me to the questionnaire") //TODO Update link
 }
 
-function updateCode(scala, value) {
-  //TODO
+function updateCode(value) {
+    code += value;
 }
 
 /* DOM manipulation helpers */
 
-function removeTextFromElement(id) {
-  let p = document.getElementById(id)
-  clearChildrenOf(p)
-}
 
-function displayTextOnElement(id, text, className, justAdd) {
-  let p = document.getElementById(id)
-  
-}
-
-function displayInstructions(text, className, justAdd) {
-  let p = document.getElementById("instructions")
-  if(!justAdd) clearChildrenOf(p) //clear text if there was text before & if not just adding text
-  if(!text) return
-  const t = document.createTextNode(text) //create new text
-  if(className) t.classList.add(className);
-  p.appendChild(t)
+function clearText() {
+    let p = document.getElementById("game-text")
+    clearChildrenOf(p) //clear text if there was text before & if not just adding text
 }
 
 function displayText(text) {
-  let p = document.getElementById("game-text")
-  clearChildrenOf(p) //clear text if there was text before & if not just adding text
-  if(!text) return
-  const t = document.createTextNode(text) //create new text
-  p.appendChild(t)
+    let gameText = document.getElementById("game-text");
+    if (!text) return;
+    const p = document.createElement("p");
+    const t = document.createTextNode(text); //create new text
+    p.appendChild(t)
+    gameText.appendChild(p);
 }
 
-function clearChildrenOf(el) {
-  const children = el.childNodes;
-  if(children && children.length > 0) {
-    children.forEach(child => el.removeChild(child));
-  }
+function clearOptions(){
+    let gameChoiceDiv = document.getElementById("game-choices")
+    clearChildrenOf(gameChoiceDiv)  //clear buttons if there were buttons before
+}
+
+function clearChildrenOf(node) {
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);
+    }
 }
 
 function addLink(url, text) {
-  let p = document.getElementById("game-text")
-  let a = document.createElement("a")
-  a.href=url
-  a.text = (text) ? text : url
-  a.target="_blank"
-  a.classList.add("button");
-  p.append(a)
+    let gameTextDiv = document.getElementById("game-text")
+    let a = document.createElement("a")
+    a.href = url
+    a.text = (text) ? text : url
+    a.target = "_blank"
+    a.classList.add("button");
+    gameTextDiv.append(a)
 }
 
 /* Buttons */
 
-function createNextButton() {
-  let ul = document.getElementById("game-choices")
-  clearChildrenOf(ul)  //clear buttons if there were buttons before
-  ul.appendChild(getButton("Continue", () => nextSituation())) //create new button
+function createContinueButton() {
+    let ul = document.getElementById("game-choices")
+    ul.appendChild(createButton("Continue", () => nextSituation())) //create new button
 }
 
-function createOptionButtons(options) {
-  let div = document.getElementById("game-choices")
-  clearChildrenOf(div)  //clear buttons if there were buttons before
-  options.forEach(s => {  //create new buttons
-    div.appendChild(getButton(s.text, () => {
-      updateCode(s.scala, s.value)
-      displayText(s.response)
-      createNextButton()
-    }))
-  })
-  div.classList.add("button-collection");
+function displayOptions(options) {
+    let gameChoiceDiv = document.getElementById("game-choices")
+    clearChildrenOf(gameChoiceDiv)  //clear buttons if there were buttons before
+    options.forEach(s => {  //create new buttons
+        gameChoiceDiv.appendChild(createButton(s.text, () => {
+            updateCode(s.value);
+            nextSituation();
+        }))
+    })
+    gameChoiceDiv.classList.add("button-collection");
 }
 
-function getButton(text, clickReaction) {
-  let b = document.createElement("button")
+function createButton(text, clickReaction) {
+    let b = document.createElement("button")
 
-  const t = document.createTextNode(text)
-  b.appendChild(t)
+    const t = document.createTextNode(text)
+    b.appendChild(t)
 
-  // b.value = value;
-  b.onclick = clickReaction
-  b.classList.add("button");
+    // b.value = value;
+    b.onclick = clickReaction
+    b.classList.add("button");
 
-  return b
+    return b
 }
--- scripts/situations.js
+++ scripts/situations.js
@@ -1,35 +1,58 @@
-const situations = {
-  0: {
-    text: "",
-    scala: "",
-    options: [
-       {
-        text: "label0",
-        value: -5,
-        response: "response text"
-      },
-      {
-        text: "label1",
-        value: -4,
-        response: "response text"
-      },
-      {
-        text: "label2",
-        value: -3,
-        response: "response text"
-      },
-      {
-        text: "label3",
-        value: -2,
-        response: "response text"
-      },
-      {
-        text: "label4",
-        value: -1,
-        response: "response text"
-      }
-    ]
-  }
-}
+const situations = [
+    {
+        text: "You come upon a cave. Your hear a deep growling from inside the cave. What are you doing",
+        scala: "empathy",
+        options: [
+            {
+                text: "You are on an important mission, you keep going",
+                value: 1,
+            },
+            {
+                text: "You sneak upon the cave to gather more information",
+                value: 3,
+            },
+            {
+                text: "you enter the cave curiously",
+                value: 5,
+            }
+        ]
+    },
+    {
+        text: "You´re distracted for a moment. That’s enough to fall into a big pound of mud you oversaw. Your companions are mockingly laughing loudly. What ist your action?",
+        scala: "empathy",
+        options: [
+            {
+                text: "You demand to be freed immediately",
+                value: 1,
+            },
+            {
+                text: "you throw some mud at them to make it square",
+                value: 3,
+            },
+            {
+                text: "you laugh humorfully with them and try to get out of the hole",
+                value: 5,
+            }
+        ]
+    },
+    {
+        text: "You approach an camp of Ogres, there is no wqay around. Battle is the only option. How will you succed?",
+        scala: "orderliness",
+        options: [
+            {
+                text: "You´ll storm their camp and get them by surprise and overwhelming fighting power",
+                value: 1,
+            },
+            {
+                text: "You prepare for battle. You get prepared as best as you can but what will happen happens",
+                value: 3,
+            },
+            {
+                text: "You have the perfect masterplan. The mage in your entourage tries to losen the bolders on the cliff and the rouge will lay some traps. There is no possibility you´ll lose",
+                value: 5,
+            }
+        ]
+    }
+]
 
 export {situations};
